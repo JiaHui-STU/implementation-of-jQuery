2709699ff21f517b561d762aadff49706dc32823
GuangJun <547652008@qq.com>
2016-05-10 17:04:04 +0800

    fixed:show

--- 01.show与链式调用/index.js
+++ 01.show与链式调用/index.js
@@ -1,6 +1,5 @@
 (function(window, undefined){
 
-
 	function jQuery(sel){
 		return new jQuery.prototype.init(sel);
 	}
@@ -13,11 +12,6 @@
 				var nodeList = document.querySelectorAll(sel);
 				Array.prototype.forEach.call(nodeList, function(val, i){
 					that[i] = val;
-					//jQuery使用其cache机制存储信息，这里简化一下
-					//直接挂载在对应的dom下
-					if(!val.oldDisplay){
-						val.oldDisplay = getDisplay(val);
-					}
 				})
 				this.selector = sel;
 				this.length = nodeList.length;
@@ -26,17 +20,38 @@
 		
 		show: function(){
 			Array.prototype.forEach.call(this, function(node){
-				node.style.display = node.oldDisplay;
+				//if(node.style) continue; //textnode没有style
+				
+				//删除style上的display:none
+				var display = node.style.display;
+				if(display === 'none'){
+					//dispaly置为空后，css如果有display则css的生效
+					//否则默认的生效
+					node.style.display = '';
+				}
+
+				//元素display值为非默认值情况，需要还原为oldDisplay：div->display:inline-block
+				//或 检测css上的display是否为none
+				if(node.style.display==='' || isHidden(node)){
+					//有oldDispaly则设置
+					if(node.oldDisplay) node.style.display = node.oldDisplay;
+					//没有则设置为元素默认值或元素当前值
+					else node.style.display = getDisplay(node);
+				}
 			})
 			
 			//链式调用
-			console.log('call show');
 			return this;
 		},
 		
 		hide: function(){
 			Array.prototype.forEach.call(this, function(node){
-				node.style.display = 'none';
+				if(!isHidden(node)) {
+					//jQuery使用其cache机制存储信息，这里简化一下
+					//直接挂载在对应的dom下
+					node.oldDisplay = getDisplay(node);
+					node.style.display = 'none';
+				}
 			})
 			
 			return this;
@@ -46,7 +61,7 @@
 	function getDisplay(node){
 		var display = window.getComputedStyle(node, null).getPropertyValue('display');
 		
-		if(display === ''){
+		if(display === 'none'){
 			var dom = document.createElement(node.nodeName);
 			//插入到body中
 			document.body.appendChild(dom);
@@ -55,6 +70,11 @@
 			document.body.removeChild(dom);
 		}
 		return display;
+	}
+	
+	function isHidden(node) {
+		//忽略未append进document的元素这种隐藏情况：$('<div>block</div>')未append
+		return window.getComputedStyle(node, null).getPropertyValue('display') === 'none';
 	}
 	
 	jQuery.prototype.init.prototype = jQuery.prototype;
--- README.md
+++ README.md
@@ -1,23 +1,19 @@
-学习目的：框架+学习API+有一些API想不通是怎么实现的
-
-框架与组成
-链式调用
-$ $.fn $.init 之间的关系【$下的方法内部使用，$.fn外部使用】
-$.extend深拷贝、浅拷贝
-$(document).ready(function(){}) 原理、与window.onload区别
-    DOMContentLoaded，load
-
-
-isNumber, isFunction【typeof缺陷】
-$("<li>1</li><li>2</li>") ，嵌套呢？
-
-
-promise deffer
-
+###学习目的：框架+学习API+有一些API想不通是怎么实现的
+1. 框架
+2. show => 如何获取Element的默认属性？createElement=>appendTo=>getDisplay<br>
+   保存进缓存  重复利用
+3. 链式调用
+4. isNumber, isFunction【typeof缺陷】  
+5. $(document).ready(function(){}) 原理、与window.onload区别<br>
+        DOMContentLoaded，load
+6. promise deffer<br>
 ---------------------------event---------------------------------------
-mouseenter mouseleave兼容【relateTarget】vol70
-on委托事件 vol70左右 【绑定两个，执行顺序有bug?】
-trigger如何触发冒泡操作： events[fatherDom]['click'] vol71
+7. mouseenter mouseleave兼容【relateTarget】vol70
+8. on委托事件 vol70左右 【绑定两个，执行顺序有bug?】
+9. trigger如何触发冒泡操作： events[fatherDom]['click'] vol71
 
----------------------------CSS操作---------------------------------------
-show => 如何获取Element的默认属性  createElement=>appendTo=>getDisplay  保存进缓存  重复利用
+
+<br><br>
+$ $.fn $.init 之间的关系【$下的方法内部使用，$.fn外部使用】<br>
+$.extend深拷贝（递归）、浅拷贝<br>
+`$("<li>1</li><li>2</li>")` ，嵌套呢？
